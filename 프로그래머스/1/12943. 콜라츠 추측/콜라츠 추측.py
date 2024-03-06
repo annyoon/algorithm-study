58fdbff5d3b19df0c92fa5f2e67afaaa6e09b800
Seoan Yoon <earthdyoon@gmail.com>
2024-03-06 10:48:42 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,6 @@
+def solution(num):
+    answer = 0
+    while num != 1 and answer < 500:
+        num = num // 2 if num % 2 == 0 else num * 3 + 1
+        answer += 1
+    return answer if answer != 500 else -1
