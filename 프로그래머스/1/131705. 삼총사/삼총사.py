b02d709a04946a7bcfdb7b43177c6d7377ddd6f6
Seoan Yoon <earthdyoon@gmail.com>
2024-03-07 16:36:16 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,9 +1,18 @@
-from itertools import combinations
+answer = 0
 
 def solution(number):
-    answer = 0
-    for d in combinations(number, 3):
-        if sum(d) == 0:
-            answer += 1
-            
+    combinations(number, 0, [])
     return answer
+
+def combinations(number, start, picked):
+    global answer
+    
+    if len(picked) == 3:
+        if sum(picked) == 0:
+            answer += 1
+        return
+    
+    for i in range(start, len(number)):
+        picked.append(number[i])
+        combinations(number, i + 1, picked)
+        picked.pop()
