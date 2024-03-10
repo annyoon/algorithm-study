b00a3d726da0d570d59e509c46012b53d8517897
Seoan Yoon <earthdyoon@gmail.com>
2024-03-10 16:31:11 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,12 +1,9 @@
 def solution(cards1, cards2, goal):
-    idx1 = 0
-    idx2 = 0
-    
     for g in goal:
-        if idx1 < len(cards1) and cards1[idx1] == g:
-            idx1 += 1
-        elif idx2 < len(cards2) and cards2[idx2] == g:
-            idx2 += 1
+        if len(cards1) and cards1[0] == g:
+            cards1.pop(0)
+        elif len(cards2) and cards2[0] == g:
+            cards2.pop(0)
         else:
             return 'No'
     return 'Yes'
