a4253026e7d08270d7a4e88af8633fb87c870db0
Seoan Yoon <earthdyoon@gmail.com>
2024-03-18 04:24:37 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -0,0 +1,31 @@
+import math
+
+def solution(str1, str2):
+    s1 = make_2_word(str1)
+    s2 = make_2_word(str2)
+    print(s1,s2)
+    if s1 == [] and s2 == []:
+        return 65536
+
+    s1_copy = s1.copy()
+    s2_copy = s2.copy()
+    
+    inter = []
+    for i in s1:
+        if i in s2_copy:
+            inter.append(i)
+            s1_copy.remove(i)
+            s2_copy.remove(i)
+            
+    union = inter + s1_copy + s2_copy
+
+    answer = math.floor((len(inter) / len(union)) * 65536)
+    return answer
+
+def make_2_word(s):
+    s = s.upper()
+    a = []
+    for i in range(len(s) - 1):
+        if s[i:i + 2].isalpha():
+            a.append(s[i:i + 2])
+    return a
