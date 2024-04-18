98af77af5733118bdce0d017d61678eb0f3fd887
Seoan Yoon <earthdyoon@gmail.com>
2024-04-18 09:58:12 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,36 +1,17 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    # 다리 길이가 1일 경우
-    if bridge_length == 1:
-        answer = len(truck_weights) + 1
-        return answer
+    bridge = deque([0] * bridge_length, maxlen = bridge_length)
+    answer, cur, i = 0, 0, 0
     
-    # 트럭이 1대일 경우
-    if len(truck_weights) == 1:
-        answer = weight + 1
-        return answer
-    
-    # 그 외 경우
-    bridge = deque([0] * (bridge_length - 1))
-    bridge.append(truck_weights[0])
-    bridge_sum = truck_weights[0]
-    time = 1
-    
-    for i in range(1, len(truck_weights)):
-        truck = bridge.popleft()
-        bridge_sum -= truck
-        time += 1
-        
-        while bridge_sum + truck_weights[i] > weight:
+    while i < len(truck_weights):
+        if cur + truck_weights[i] - bridge[0] <= weight:
+            cur += (truck_weights[i] - bridge[0])
+            bridge.append(truck_weights[i])
+            i += 1
+        else:
+            cur -= bridge[0]
             bridge.append(0)
-            truck = bridge.popleft()
-            bridge_sum -= truck
-            time += 1
-            
-        bridge.append(truck_weights[i])
-        bridge_sum += truck_weights[i]
+        answer += 1
         
-    answer = time + bridge_length
-
-    return answer
+    return answer + bridge_length
