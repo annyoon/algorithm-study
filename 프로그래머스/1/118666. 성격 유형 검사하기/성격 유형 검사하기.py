c2a4941b8640d226ddfe85232439be38689a8cff
Seoan Yoon <earthdyoon@gmail.com>
2024-04-22 00:40:57 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,18 +1,17 @@
 def solution(survey, choices):
-    person = {'R':0, 'T':0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
-    score = {1:[0, 3], 2:[0, 2], 3:[0, 1], 4:[0, 0], 5:[1, 1], 6:[1, 2], 7:[1, 3]}
+    answer = ''
+    type_dic = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
+    score = {1: [0, 3], 2: [0, 2], 3: [0, 1], 4: [0, 0], 5: [1, 1], 6: [1, 2], 7: [1, 3]}
     
     for i in range(len(survey)):
         s = score[choices[i]]
-        person[survey[i][s[0]]] += s[1]
-        
-    result = ''
+        type_dic[survey[i][s[0]]] += s[1]
     
-    for p in ['RT', 'CF', 'JM', 'AN']:
-        num = max(person[p[0]], person[p[1]])
-        for alp in p:
-            if person[alp] == num:
-                result += alp
-                break
+    for type_c in ['TR', 'FC', 'MJ', 'NA']:
+        max_num = max(type_dic[type_c[0]], type_dic[type_c[1]])
+        for c in type_c:
+            if type_dic[c] == max_num:
+                tmp = c
+        answer += tmp
     
-    return result
+    return answer
