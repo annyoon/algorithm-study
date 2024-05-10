6a3532bf43896ce75161d7834981f8194876ad47
Seoan Yoon <earthdyoon@gmail.com>
2024-05-11 02:19:24 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 16.70 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,27 +1,46 @@
-from itertools import combinations
-
 def solution(orders, course):
-    result = []
+    answer = []
+    dic = {}
     
-    for c in course:
-        dic = {}
-        
-        for order in orders:
-            order = sorted(list(order))
-            for combi in list(combinations(order, c)):
-                combi = ''.join(list(combi))
-                if combi in dic:
-                    dic[combi] += 1
-                else:
-                    dic[combi] = 1
-                    
-        dic = dict(sorted(dic.items()))
-        if len(dic) > 0:
-            max_num = max(dic.values())
+    def combination(i, menu, menu_num, combo):
+        if len(combo) == menu_num:
+            combo_str = ""
             
-            if max_num >= 2:
-                for d in dic:
-                    if dic[d] == max_num:
-                        result.append(d)
-                    
-    return sorted(result)
+            for c in combo:
+                combo_str += c
+                
+            menu_combo.append(combo_str)
+            return
+        
+        for num in range(i, len(menu)):
+            combo.append(menu[num])
+            combination(num + 1, menu, menu_num, combo)
+            combo.pop()
+    
+    for course_num in course:
+        for order in orders:
+            order_list = list(order)
+            order_list.sort()
+            
+            menu_combo = []
+            combination(0, order_list, course_num, [])
+            
+            for mc in menu_combo:
+                if mc in dic:
+                    dic[mc] += 1
+                else:
+                    dic[mc] = 1
+    
+    for course_num in course:
+        dic_num = {}
+        for d in dic:
+            if len(d) == course_num:
+                if dic[d] >= 2:
+                    dic_num[d] = dic[d]
+            
+        tmp = [k for k,v in dic_num.items() if max(dic_num.values()) == v]
+        
+        for t in tmp:
+            answer.append(t)
+    
+    return sorted(answer)
