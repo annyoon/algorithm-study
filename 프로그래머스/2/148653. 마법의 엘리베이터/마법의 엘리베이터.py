c8c88e510a1a94dda01f8f111253d7a631681949
Seoan Yoon <earthdyoon@gmail.com>
2024-06-12 00:17:55 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,16 +1,12 @@
 def solution(storey):
+    length = len(str(storey)) - 1
+
+    if length == 0:
+        return min(storey, 11 - storey)
+
+    sub = 10 ** (length + 1) - storey
     
-    def solve(num):
-        length = len(str(num)) - 1
-        
-        if length == 0:
-            return min(num, 11 - num)
-        
-        sub = 10 ** (length + 1) - num
-        
-        result1 = num // (10 ** length) + solve(num % (10 ** length))
-        result2 = 1 + sub // (10 ** length) + solve(sub % (10 ** length))
-        
-        return min(result1, result2)
-    
-    return solve(storey)
+    result1 = storey // (10 ** length) + solution(storey % (10 ** length))
+    result2 = 1 + sub // (10 ** length) + solution(sub % (10 ** length))
+
+    return min(result1, result2)
