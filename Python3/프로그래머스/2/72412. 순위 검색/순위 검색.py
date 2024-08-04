69ef8bdc027ed7e12fd631ea40473939c025a1b3
Seoan Yoon <earthdyoon@gmail.com>
2024-08-05 02:37:50 +0900

    [level 2] Title: 순위 검색, Time: 757.54 ms, Memory: 61.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ Python3/프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -1,12 +1,12 @@
 # 답 참고
 
 def solution(info, query):
-    dic = dict()
+    data = dict()
     for a in ['cpp', 'java', 'python', '-']:
         for b in ['backend', 'frontend', '-']:
             for c in ['junior', 'senior', '-']:
                 for d in ['chicken', 'pizza', '-']:
-                    dic.setdefault((a, b, c, d), list())
+                    data.setdefault((a, b, c, d), list())
 
     for i in info:
         i = i.split()
@@ -14,26 +14,24 @@ def solution(info, query):
             for b in [i[1], '-']:
                 for c in [i[2], '-']:
                     for d in [i[3], '-']:
-                        dic[(a, b, c, d)].append(int(i[4]))
-
-    for k in dic:
-        dic[k].sort()
+                        data[(a, b, c, d)].append(int(i[4]))
+                        
+    for k in data:
+        data[k].sort()
 
     answer = list()
 
     for q in query:
         q = q.split()
-        scores = dic[(q[0], q[2], q[4], q[6])]
-        std = int(q[7])
-        l, r = 0, len(scores)
-        
+        scores = data[(q[0], q[2], q[4], q[6])]
+        wanted = int(q[7])
+        l,r = 0, len(scores)
         while l < r:
-            mid = (l + r) // 2
-            if scores[mid] >= std:
-                r = mid
+            middle = (l + r)//2
+            if scores[middle] >= wanted:
+                r = middle
             else:
-                l = mid + 1
-                
-        answer.append(len(scores) - l)
+                l = middle + 1
+        answer.append(len(scores)-l)
         
     return answer
